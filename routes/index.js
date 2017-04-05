a6e5c65cd3d75b7531020e97c8bce4090817b3b2
Alex Towli <alextowli@hotmail.co.uk>
2017-04-05 20:18:22 +0100

    initial styling and routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET landing page of app. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'unwind.js' });
+});
+
+/* GET landing page of app. */
+router.get('/paint', function(req, res, next) {
+  res.render('canvas/index', { title: 'paint' });
+});
+
+router.get('/write', function(req, res, next) {
+  res.render('editor/index', { title: 'write' });
 });
 
 module.exports = router;
