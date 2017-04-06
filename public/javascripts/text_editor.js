498bd41dec8aa13ba904ab1e55ef5c4b42da4403
Alex Towli <alextowli@hotmail.co.uk>
2017-04-06 15:18:09 +0100

    listen to input event and add sinon.js dependency

--- public/javascripts/text_editor.js
+++ public/javascripts/text_editor.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 	var pad = $('#writing-pad');
+	var padContent = "";
+
 	pad.attr('placeholder', "Tell us your story...");
+
+	/* On pad-content change, begin auto-saving process */
+	pad.on('input',function() {
+		padContent = pad.val();
+		console.log(padContent);
+	});
+
 });
