713b7963ce7fec2a4281a640c00e3671c0dbc866
Alex Towli <alextowli@hotmail.co.uk>
2017-04-06 16:10:16 +0100

    initial auto-save feature test

--- public/javascripts/text_editor.js
+++ public/javascripts/text_editor.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
 	var pad = $('#writing-pad');
 	var padContent = "";
+	var isSaving = false;
 
 	pad.attr('placeholder', "Tell us your story...");
 
 	/* On pad-content change, begin auto-saving process */
 	pad.on('input',function() {
+		if (isSaving)
+			return;
+
 		padContent = pad.val();
+		
+		isSaving = true;
 		$.post('/write', { data: padContent }).done(function(data) {
 			console.log('Post request complete.');
+			setTimeout(function() {
+				isSaving = false;
+				console.log("Timeout expired - ready to save again.");
+			}, 2000);
 		});
+	
 	});
 
 });
